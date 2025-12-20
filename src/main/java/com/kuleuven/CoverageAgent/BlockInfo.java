331360a25cdff36ca3d2f2ea8180dd3ace1a8764
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-20 23:22:19 +0100

    Update stmt to stmtId

--- src/main/java/com/kuleuven/CoverageAgent/BlockInfo.java
+++ src/main/java/com/kuleuven/CoverageAgent/BlockInfo.java
@@ -5,6 +5,6 @@ public record BlockInfo(
         String className,
         String methodName,
         String methodDescriptor,
-        String stmt,
+        String stmtId,
         int lineNumber
 ) {}
