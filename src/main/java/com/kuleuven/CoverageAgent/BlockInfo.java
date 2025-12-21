a26736490fcd4948a389fc36dceb1a949ce55894
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-21 11:00:59 +0100

    Add blockId to BlockInfo

--- src/main/java/com/kuleuven/CoverageAgent/BlockInfo.java
+++ src/main/java/com/kuleuven/CoverageAgent/BlockInfo.java
@@ -2,6 +2,7 @@ package com.kuleuven.CoverageAgent;
 
 // Should match the BlockInfo of the call-graph-generator
 public record BlockInfo(
+        int blockId,
         String className,
         String methodName,
         String methodDescriptor,
